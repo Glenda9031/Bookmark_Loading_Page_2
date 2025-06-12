110eed894d47a99d12af607fbfbc710551e51434
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2025-06-12 17:41:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,5 +12,30 @@ tabHeader.addEventListener("click", function (e) {
     tabItem.forEach((t) => t.classList.remove("active"));
     tabContent.forEach((a) => a.classList.remove("active"));
 
+    tabHeader.addEventListener("click", function (e) {
+    const clicked = e.target.closest(".tab-header-item");
+
+    if (!clicked) return;
+    clicked.classList.add("active");
+
+    tabItem.forEach((t) => t.classList.remove("active"));
+    tabContent.forEach((a) => a.classList.remove("active"));
+    clicked.classList.add("active");
+
+    document
+        .querySelector(`.tab-content-item[data-tab="${clicked.dataset.tab}"]`)
+        .classList.add("active");
+    });
+
+    const faqContainer = document.querySelector(".faq-list");
+    const faqItem = document.querySelectorAll(".faq-item");
+
+    faqContainer.addEventListener("click", function (e) {
+        const clicked = e.target.closest(".faq-header");
+        if (!clicked) return;
+
+        clicked.parentElement.classList.toggle("active");
+    });
+
     
 })
